d6efb192232f0f36a4b4fe63a9371b35d021505d
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-11 01:56:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  SELECT books, titles, years FROM books ORDER BY books.year;
+  SELECT books.title, books.years FROM books 
+  JOIN series
+  ON series.book_id = books.id
+  ORDER BY books.year;
 end
 
 def select_name_and_motto_of_char_with_longest_motto
