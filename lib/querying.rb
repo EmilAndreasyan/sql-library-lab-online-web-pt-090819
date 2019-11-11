a4951a42983169a0dad17f270be8a824405452cd
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-11 02:26:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT title FROM series COUNT
+  SELECT title FROM series COUNT * ORDER BY title DESC LIMIT 
 end
 
 def select_character_names_and_number_of_books_they_are_in
