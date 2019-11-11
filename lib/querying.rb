186dbf833fa2ab6e9cdc4abafe85406d62c1b8e6
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-11 02:29:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 
 def select_series_title_with_most_human_characters
   SELECT title FROM series
-  JOIN 
+  JOIN books
+  ON books.id = 
 end
 
 def select_character_names_and_number_of_books_they_are_in
