755356c7540d45f049a519b18a60308959f501c4
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-11 02:14:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 def select_name_and_series_subgenres_of_authors
   SELECT authors.name, subgenres.name FROM authors
   JOIN series 
-  ON 
+  ON series.author_id = authors.id AND series.
   "Write your SQL query here"
 end
 
